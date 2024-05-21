f2a5f327de2b532db4ba47bb23900592b38b2642
David Poza <dave_pm@outlook.com>
2024-05-21 16:02:51 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import DynamicClock from './Component/dynamic-clock';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-[#1E1E1E]">
       <header className="App-header">
         <DynamicClock/>
       </header>
